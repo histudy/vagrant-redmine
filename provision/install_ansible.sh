f0f04a2c5967938463f00fddd49760cce1760fef
wate <pc.poisoning@gmail.com>
2018-07-16 20:09:52 +0900

    Ansibleインストール用スクリプトを調整

--- provision/install_ansible.sh
+++ provision/install_ansible.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
+if [ ! -f /usr/bin/dirmngr ]; then
+  apt-get install -y dirmngr
+fi
 if [ ! -f /etc/apt/sources.list.d/ansible.list ]; then
   echo "deb http://ppa.launchpad.net/ansible/ansible/ubuntu trusty main">/etc/apt/sources.list.d/ansible.list
-  if [ ! -f /usr/bin/dirmngr ]; then
-    apt-get install -y dirmngr
-  fi
   apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 93C4A3FD7BB9C367
   apt-get update
 fi
