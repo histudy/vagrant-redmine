edbba88b844123c2b351a93ebc882f4d3fd0f1e7
wate <pc.poisoning@gmail.com>
2018-09-03 01:40:16 +0900

    Squashed 'provision/roles/redmine/' changes from 87f746a..59bc0ad
    59bc0ad プラグインの設定の流し込みに対応
    
    git-subtree-dir: provision/roles/redmine
    git-subtree-split: 59bc0ad19eb4ee38224c6f9e057a0663fe076c33

--- spec/config_spec.rb
+++ spec/config_spec.rb
@@ -3,20 +3,20 @@ require 'spec_helper'
 describe file(property['redmine_home'] + '/.bundle/config') do
   it { should exist }
   it { should be_file }
-  it { should contain 'BUNDLE_PATH: "vendor/bundle"' }
-  it { should contain 'BUNDLE_WITHOUT: "development:test"' }
+  its(:content) { should match 'BUNDLE_PATH: "vendor/bundle"' }
+  its(:content) { should match 'BUNDLE_WITHOUT: "development:test"' }
 end
 
 describe file(property['redmine_home'] + '/config/database.yml') do
   it { should exist }
   it { should be_file }
   redmine_db_cfg = property['redmine_db_cfg']['production']
-  it { should contain "adapter: #{redmine_db_cfg['adapter']}" }
-  it { should contain "host: #{redmine_db_cfg['host']}" }
-  it { should contain "database: #{redmine_db_cfg['database']}" }
-  it { should contain "username: #{redmine_db_cfg['username']}" }
-  it { should contain "password: #{redmine_db_cfg['password']}" }
-  it { should contain "encoding: #{redmine_db_cfg['encoding']}" }
+  its(:content) { should match "adapter: #{redmine_db_cfg['adapter']}" }
+  its(:content) { should match "host: #{redmine_db_cfg['host']}" }
+  its(:content) { should match "database: #{redmine_db_cfg['database']}" }
+  its(:content) { should match "username: #{redmine_db_cfg['username']}" }
+  its(:content) { should match "password: #{redmine_db_cfg['password']}" }
+  its(:content) { should match "encoding: #{redmine_db_cfg['encoding']}" }
 end
 
 describe file(property['redmine_home'] + '/config/configuration.yml') do
@@ -27,17 +27,19 @@ describe file(property['redmine_home'] + '/config/configuration.yml') do
   it { should contain "delivery_method: :#{email_delivery['delivery_method']}" }
   if email_delivery['delivery_method'] == :smtp
     smtp_settings = email_delivery['smtp_settings']
-    it { should contain "address: #{smtp_settings['address']}" }
-    it { should contain "port: #{smtp_settings['port']}" }
-    it { should contain "authentication: #{smtp_settings['authentication']}" } if smtp_settings.key?('authentication')
-    it { should contain "domain: #{smtp_settings['domain']}" } if smtp_settings.key?('domain')
-    it { should contain "user_name: #{smtp_settings['user_name']}" } if smtp_settings.key?('user_name')
-    it { should contain "password: #{smtp_settings['password']}" } if smtp_settings.key?('password')
+    its(:content) { should match "address: #{smtp_settings['address']}" }
+    its(:content) { should match "port: #{smtp_settings['port']}" }
+    if smtp_settings.key?('authentication')
+      its(:content) { should match "authentication: #{smtp_settings['authentication']}" }
+    end
+    its(:content) { should match "domain: #{smtp_settings['domain']}" } if smtp_settings.key?('domain')
+    its(:content) { should match "user_name: #{smtp_settings['user_name']}" } if smtp_settings.key?('user_name')
+    its(:content) { should match "password: #{smtp_settings['password']}" } if smtp_settings.key?('password')
     if smtp_settings.key?('enable_starttls_auto')
       enable_starttls_auto = smtp_settings['authentication'] ? 'true' : 'false'
-      it { should contain "enable_starttls_auto: #{enable_starttls_auto}" }
+      its(:content) { should match "enable_starttls_auto: #{enable_starttls_auto}" }
     end
   end
-  it { should contain "attachments_storage_path: #{redmine_cfg['attachments_storage_path']}" }
-  it { should contain "rmagick_font_path: #{redmine_cfg['rmagick_font_path']}" }
+  its(:content) { should match "attachments_storage_path: #{redmine_cfg['attachments_storage_path']}" }
+  its(:content) { should match "rmagick_font_path: #{redmine_cfg['rmagick_font_path']}" }
 end
